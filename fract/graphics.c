04099d10bf64ae61273b07d0632c09eee7da2d60
Jeremy Karagania <jeremykaragania@icloud.com>
2025-06-25 12:09:08 +0800

    Add pixel drawing graphics helpers

--- fract/graphics.c
+++ fract/graphics.c
@@ -5,9 +5,9 @@ EFI_GRAPHICS_OUTPUT_MODE_INFORMATION* gop_info;
 UINTN gop_info_size;
 
 /*
-* init_graphics initializes the Graphics Output Protocol (GOP), initializing
-* global objects for graphics operations.
-*/
+ * init_graphics initializes the Graphics Output Protocol (GOP), initializing
+ * global objects for graphics operations.
+ */
 EFI_STATUS init_graphics() {
   EFI_STATUS status;
 
@@ -26,4 +26,27 @@ EFI_STATUS init_graphics() {
   }
 
   return EFI_SUCCESS;
+}
+
+/*
+ * get_pixel returns a copy of the pixel in the framebuffer from its x
+ * coordinate, `x` and y coordinate, `y`.
+ */
+uint32_t get_pixel(uint32_t x, uint32_t y) {
+  return ((uint32_t*)gop->Mode->FrameBufferBase)[y * gop_info->HorizontalResolution + x];
+}
+
+/*
+ * set_pixel sets a pixel in the framebufffer to value `val` from its x
+ * coordinate, `x` and y coordinate, `y`. If either coordinate is out of
+ * bounds, then nothing is set. -1 is returned on failure and 0 on success.
+ */
+int set_pixel(uint32_t x, uint32_t y, uint32_t val) {
+  if (x >= gop_info->HorizontalResolution || y >= gop_info->HorizontalResolution) {
+    return -1;
+  }
+
+  ((uint32_t*)gop->Mode->FrameBufferBase)[y * gop_info->HorizontalResolution + x] = val;
+
+  return 0;
 }
--- fract/graphics.h
+++ fract/graphics.h
@@ -10,4 +10,7 @@ extern UINTN gop_info_size;
 
 EFI_STATUS init_graphics();
 
+uint32_t get_pixel(uint32_t x, uint32_t y);
+int set_pixel(uint32_t x, uint32_t y, uint32_t val);
+
 #endif
