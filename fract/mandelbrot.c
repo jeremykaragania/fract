bfc75307d8cbd15abc946ee097dea8bacd2dc2fc
Jeremy Karagania <jeremykaragania@icloud.com>
2025-06-30 18:53:57 +0800

    Add initial Mandelbrot set

---
+++ fract/mandelbrot.c
@@ -0,0 +1,37 @@
+#include <mandelbrot.h>
+
+void draw_mandelbrot() {
+  set_plane_x(-2, 0.47);
+  set_plane_y(-1.12, 1.12);
+
+  size_t max_iterations = 1000;
+  struct plane_point pp;
+  struct screen_point sp;
+  struct plane_point z;
+
+  for (uint32_t x = 0; x < screen_width; ++x) {
+    for (uint32_t y = 0; y < screen_height; ++y) {
+      z.x = 0.0f;
+      z.y = 0.0f;
+      sp.x = x;
+      sp.y = y;
+      pp = screen_to_plane(sp);
+
+
+      for (size_t i = 0; i < max_iterations; ++i) {
+        z.x = z.x * z.x - z.y * z.y + pp.x;
+        z.y = 2 * z.x * z.y + pp.y;
+        if (z.x * z.x + z.y * z.y >= 4) {
+          uint8_t p = (255.0f * ((float)i / (float)max_iterations));
+          set_pixel(sp, p & (p >> 8) & (p >> 16));
+          break;
+        }
+
+        if (i == max_iterations - 1) {
+          set_pixel(sp, 0xffffff);
+          break;
+        }
+      }
+    }
+  }
+}
---
+++ fract/mandelbrot.h
@@ -0,0 +1,9 @@
+#ifndef MANDELBROT_H
+#define MANDELBROT_H
+
+#include <graphics.h>
+#include <plane.h>
+
+void draw_mandelbrot();
+
+#endif
